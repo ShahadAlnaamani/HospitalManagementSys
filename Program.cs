f2095073b1bb3d574d05e122ace072cfd4ac60db
Shahad <Shahadalnaamani@outlook.com>
2024-09-29 14:56:53 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalManagementSys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
