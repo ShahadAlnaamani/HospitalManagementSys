c03217c424a3fd153cbbeeb0b985c69881176774
Shahad <Shahadalnaamani@outlook.com>
2024-09-29 15:22:08 +0400

    Created parent class person (patient and doctor will inherit it)

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace HospitalManagementSys
+{
+	public abstract class Person
+	{
+		public string Name { get; private set; }
+		public double Age { get; private set; }
+		public string Gender { get; private set; }
+
+		public Person(string name, DateTime DOB, string gender)
+		{
+			Name = name;
+			TimeSpan timeSpan = DateTime.Today - DOB;
+			Age = timeSpan.TotalDays;
+			Gender = gender;
+		}
+
+		public virtual void DisplayInfo()
+		{
+			Console.WriteLine($"Name: {Name}, Age: {Age}, Gender: {Gender}");
+		}
+	}
+}
