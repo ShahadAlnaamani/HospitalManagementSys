55fcb98240637c5f8091af1035b67fef1ff51df7
Shahad <Shahadalnaamani@outlook.com>
2024-09-30 07:30:26 +0400

    Added functionality to doctor class

---
+++ Doctor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace HospitalManagementSys
+{
+    public abstract class Doctor : Person
+    {
+        //Attributes
+        public int DoctorID { get; private set; }
+        public string Specialization { get; private set; }
+        public List<Patient> PatientsList { get; private set; } //list of patient ids
+
+        //Constructor
+        public Doctor(string name, int age, string gender, int doctorID, string specialization) : base(name, age, gender)
+        {
+            DoctorID = doctorID;
+            Specialization = specialization;    
+            PatientsList = new List<Patient>();
+        }
+
+        //Methods 
+        public void AddPatient(Patient patient)
+        { 
+            PatientsList.Add(patient);
+        }
+
+        public void RemovePatient(Patient patient)
+        {
+            PatientsList.Remove(patient);
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine();
+            base.DisplayInfo();
+            Console.WriteLine($"PatientID: {DoctorID}, Ailement {Specialization}\n");
+        }
+    }
+}
