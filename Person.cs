9853a75c52654cee76dd7661ac36011e0677052b
Shahad <Shahadalnaamani@outlook.com>
2024-09-30 07:54:01 +0400

    edited gender type to enum

--- Person.cs
+++ Person.cs
@@ -8,18 +8,28 @@ namespace HospitalManagementSys
 	{
 		public string Name { get; private set; }
 		public int Age { get; private set; }
-		public string Gender { get; private set; }
+		//public string Gender { get; private set; }
+		public enum Gender { Male, Female}
+        public Gender g { get; private set; }
 
-		public Person(string name, int age, string gender)
+        public Person(string name, int age, string gender)
 		{
 			Name = name;
 			Age = age;
-			Gender = gender;
-		}
+			
+			if (gender.Trim().ToLower() != "male")
+			{
+				g = Gender.Female;
+			}
+			else
+            {
+                g = Gender.Male;
+            }
+        }
 
 		public virtual void DisplayInfo()
 		{
-			Console.WriteLine($"Name: {Name}, Age: {Age}, Gender: {Gender}");
+			Console.WriteLine($"Name: {Name}, Age: {Age}, Gender: {g}");
 		}
 	}
 }
