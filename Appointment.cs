7f2e3a10747c640be97c054816f6fc754ca2f0a6
Shahad <Shahadalnaamani@outlook.com>
2024-09-30 08:57:10 +0400

    Added functionality to Appointment class

---
+++ Appointment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace HospitalManagementSys
+{
+    public abstract class Appointment
+    {
+        //Attributes 
+        public string PatientName { get; private set; }
+        public string DoctorName { get; private set; }
+        public DateTime? AppointmentDate { get; private set; }
+
+
+        //Constructor
+        public Appointment(string patientName, string doctorName)
+        {
+            PatientName = patientName;
+            DoctorName = doctorName;
+        }
+
+        //Methods 
+        public void ScheduleAppointment(DateTime date)
+        {
+            AppointmentDate = date;
+        }
+
+        public void CancelAppointment()
+        {
+            AppointmentDate = null;
+        }
+
+        public void GetAppointmentDetails()
+        {
+            Console.WriteLine($"Patient Name: {PatientName} | Doctor Name: {DoctorName} | Date: {AppointmentDate}");
+        }
+    }
+}
